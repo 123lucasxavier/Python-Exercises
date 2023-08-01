9dfe365d68e3f2a79fa66d79e09eef454e2dd4da
123lucasxavier <123lucasxavier@gmail.com>
2023-08-01 13:54:00 -0300

    001 to 004
    Python Exercises From 001 to 004

---
+++ ex 002.py
@@ -0,0 +1,4 @@
+day = input('What is the day of your birth?')
+month = input('What is your birth month?')
+year = input('What is the year of your birth?')
+print('You were born in',day,'/',month+'/',year)
---
+++ ex 003.py
@@ -0,0 +1,4 @@
+n1 = int (input('Enter any number'))
+n2 = int(input('Enter any other number'))
+sum = n1 + n2
+print('The sum of the number {} plus the number {} equals {}'.format(n1,n2,sum))
